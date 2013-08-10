f61505a465519e7bb398d89926689872f19d388e
ervin keith <ervin0889@yahoo.com>
2013-08-10 08:58:32 -0400

    Created a rails application and added status updates

---
+++ app/models/status.rb
@@ -0,0 +1,3 @@
+class Status < ActiveRecord::Base
+  attr_accessible :contenet, :name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Solebook::Application.config.secret_token = '78e5785e3bb4e7879b3a0ec4232e33e346ad1a09d655e69b80c1432fcc0c8b093c097cea3f519df0c24385cc8d807b2de4e46bcbfb8502194d1767dcc3f2b463'
